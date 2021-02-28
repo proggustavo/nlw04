8ec828e36df2535d397e6dd9b93e7e806f084119
proggustavo <prog.gustavosantos@gmail.com>
2021-02-28 16:31:52 -0300

    SendMail nodemail, ethereal

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,20 @@
 import { Router } from "express";
 import { UserController } from "./controllers/UserController";
 import { SurveysController } from "./controllers/SurveysController";
+import { SendMailController } from "./controllers/SendMailController";
 
 const router = Router();
 
 const userController = new UserController();
 const surveyContoller = new SurveysController();
 
+const sendMailController = new SendMailController();
+
 router.post("/users", userController.create);
+
 router.post("/survey", surveyContoller.create);
 router.get("/survey", surveyContoller.list);
+
+router.post("/sendMail", sendMailController.execute);
 
 export { router };
